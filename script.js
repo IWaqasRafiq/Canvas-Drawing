946ef3368b7e68959634cc2d85c44ddca449128c
waqas rafiq <wm66179@gmail.com>
2023-06-02 15:28:02 +0500

    Working

--- script.js
+++ script.js
@@ -9,86 +9,86 @@ saveImg = document.querySelector(".save-img")
 const ctx = canvas.getContext("2d")
 
 let prevMouseX, prevMouseY, snapshot,
-isDrawing = false,
-selectedTool = "brush",
-brushWidth = 5,
-selectedColor = "#000"
+  isDrawing = false,
+  selectedTool = "brush",
+  brushWidth = 5,
+  selectedColor = "#000"
 
-window.addEventListener("load", () =>{
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
+window.addEventListener("load", () => {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
 });
 
 const drawRec = (e) => {
-    if(!fillcolor.checked){
-       return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
-    }
-    ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+  if (!fillcolor.checked) {
+    return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+  }
+  ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
 }
 
-const startDraw = (e) =>{
-    isDrawing = true
-    prevMouseX = e.offsetX;
-    prevMouseY = e.offsetY;
-    ctx.beginPath();
-    ctx.lineWidth = brushWidth;
-    ctx.strokeStyle = selectedColor;
-    ctx.fillStyle = selectedColor;
-    snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+const startDraw = (e) => {
+  isDrawing = true
+  prevMouseX = e.offsetX;
+  prevMouseY = e.offsetY;
+  ctx.beginPath();
+  ctx.lineWidth = brushWidth;
+  ctx.strokeStyle = selectedColor;
+  ctx.fillStyle = selectedColor;
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
 const drawing = (e) => {
-    if(!isDrawing) return;
-    ctx.putImageData(snapshot, 0, 0);
+  if (!isDrawing) return;
+  ctx.putImageData(snapshot, 0, 0);
 
-    if(selectedTool === "brush" || selectedTool === "eraser"){
-        ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
-    } else if(selectedTool === "rectangle"){
-        drawRec(e);
-    }
+  if (selectedTool === "brush" || selectedTool === "eraser") {
+    ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+  } else if (selectedTool === "rectangle") {
+    drawRec(e);
+  }
 }
 
 toolBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        document.querySelector(".option", ".actve").classList.remove("active");
-        btn.classList.add("active");
-        selectedTool = btn.id;
-        console.log(selectedTool);
-    })
+  btn.addEventListener("click", () => {
+    document.querySelector(".option", ".actve").classList.remove("active");
+    btn.classList.add("active");
+    selectedTool = btn.id;
+    console.log(selectedTool);
+  })
 });
 
 sizeslider.addEventListener("change", () => brushWidth = sizeslider.value);
 
-colorBtns.forEach(btn =>{
-    btn.addEventListener("click", () =>{
-        document.querySelector(".option", ".selected").classList.remove("selected");
-        btn.classList.add("selected");    
-        selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");   
-    });
+colorBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".option", ".selected").classList.remove("selected");
+    btn.classList.add("selected");
+    selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
+  });
 });
 
-colorPicker.addEventListener("change", () =>{
-    colorPicker.parentElement.style.background = colorPicker.value
-    colorPicker.parentElement.click();
+colorPicker.addEventListener("change", () => {
+  colorPicker.parentElement.style.background = colorPicker.value
+  colorPicker.parentElement.click();
 })
 
-clearCanvas.addEventListener("click", () =>{
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+clearCanvas.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 })
 
 saveImg.addEventListener("click", () => {
-    const link = document.createElement("a");
-    link.download = '$(date.now()).jpg';
-    link.href = canvas.toDataURL();
-    link.click();
+  const link = document.createElement("a");
+  link.download = '$(date.now()).jpg';
+  link.href = canvas.toDataURL();
+  link.click();
 })
 
 canvas.addEventListener("mousedown", startDraw)
 canvas.addEventListener("mousemove", drawing)
-canvas.addEventListener("mouseup", () => isDrawing = false )
+canvas.addEventListener("mouseup", () => isDrawing = false)
 
 let prevX = 0;
 let prevY = 0;
@@ -109,9 +109,14 @@ canvas.addEventListener("touchmove", (e) => {
   const currentY = clientY;
 
   ctx.beginPath();
+  ctx.lineWidth = brushWidth;
+  ctx.strokeStyle = selectedColor;
+  ctx.fillStyle = selectedColor;
   ctx.moveTo(prevX, prevY);
-  ctx.lineTo(currentX, currentY);
-  ctx.stroke();
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  // ctx.lineTo(currentX, currentY);
+  // ctx.stroke();
 
   prevX = currentX;
   prevY = currentY;
@@ -125,15 +130,15 @@ canvas.addEventListener("touchcancel", () => {
   isDrawing = false;
 });
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
-  const checkbox = document.getElementById("checkbox")
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark")
 })
